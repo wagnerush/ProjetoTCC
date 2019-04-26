7cd310f87fbdfba13941c5fd4331a62750a831b3
Wagnerush <wagner.avell@gmail.com>
2019-04-26 20:22:34 -0300

    Inclusão de Arquivos do projeto

---
+++ paginas/cadastro.php
@@ -0,0 +1,25 @@
+<h2>Cadastro</h2>
+
+<div class="row">
+	<div class="col-md-3">
+	</div>
+
+	<div class="col-md-6">
+<form action="?pg=processar" method="POST">
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" name= "email" aria-describedby="emailHelp" placeholder="Digite seu Email">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" name= "senha" placeholder="Password">
+  </div>
+  
+  <button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+</div>
+
+<div class="col-md-3">
+	</div>
+
+</div>
---
+++ paginas/inicio.php
@@ -0,0 +1,34 @@
+<h2>Início</h2>
+
+<div class="row">
+	<div class="col-md">
+	  	<div class="card" style="width: 18rem;">
+		  <img src="https://cdn-motorshow-ssl.akamaized.net/wp-content/uploads/sites/2/2018/08/bugatti-divo.jpg" class="card-img-top" alt="Imagem de um carro">
+		  <div class="card-body">
+		    <h5 class="card-title">Card title</h5>
+		    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+		    <a href="#" class="btn btn-primary">Go somewhere</a>
+		  </div>
+		</div>
+	</div>
+	<div class="col-md">
+	  	<div class="card" style="width: 18rem;">
+		  <img src="https://cdn-motorshow-ssl.akamaized.net/wp-content/uploads/sites/2/2018/08/bugatti-divo.jpg" class="card-img-top" alt="...">
+		  <div class="card-body">
+		    <h5 class="card-title">Card title</h5>
+		    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+		    <a href="#" class="btn btn-primary">Go somewhere</a>
+		  </div>
+		</div>
+	</div>
+	<div class="col-md">
+	  	<div class="card" style="width: 18rem;">
+		  <img src="https://cdn-motorshow-ssl.akamaized.net/wp-content/uploads/sites/2/2018/08/bugatti-divo.jpg" class="card-img-top" alt="...">
+		  <div class="card-body">
+		    <h5 class="card-title">Card title</h5>
+		    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+		    <a href="#" class="btn btn-primary">Go somewhere</a>
+		  </div>
+		</div>
+	</div>
+</div>
